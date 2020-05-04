e1f6e3324abe1195ee4fcc3923b35547d7edab25
speedgirl1777 <speedgirl1777@gmail.com>
2020-05-04 16:21:08 +0800

    feat: 初始化项目

---
+++ src/stories/App.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../App';
+
+export default {
+  title: 'App',
+  component: App,
+};
+
+export const ToStorybook = () => <App />;
+
+ToStorybook.story = {
+  name: 'App',
+};
